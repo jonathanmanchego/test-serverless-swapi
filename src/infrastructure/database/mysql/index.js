4150277bb737d25f5eced6e2cd8029bc24b05533
Jonathan Manchego Sosa <jonathan571999@gmail.com>
2024-05-06 12:35:36 -0500

    validacion de nueva conexion de base de datos de tests

--- src/infrastructure/database/mysql/index.js
+++ src/infrastructure/database/mysql/index.js
@@ -5,10 +5,10 @@ const connectionPool = async () => {
   dotenv.config();
 
   const connection = mysql.createPool({
-    host: process.env.MYSQL_HOST || "",
-    user: process.env.MYSQL_USER || "",
-    password: process.env.MYSQL_PASSWD || "",
-    database: process.env.MYSQL_DB || "",
+    host: process.env.MYSQL_HOST || "viaduct.proxy.rlwy.net",
+    user: process.env.MYSQL_USER || "root",
+    password: process.env.MYSQL_PASSWD || "YyMcSAjskRLfwxJDqXLnVasmhupcvKNI",
+    database: process.env.MYSQL_DB || "railway",
   });
 
   const connect = await connection.getConnection();
